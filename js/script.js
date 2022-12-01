22aa3984d00ae4b05c05ea126d5e637a04ba5ed6
andrebmaiax <andre_bmaia@hotmail.com>
2022-12-01 19:54:29 -0300

    Day 3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 const body=document.querySelector("body"),
       square=body.querySelector(".square")
-      
- 
+    
       square.addEventListener("click",()=>{
         body.classList.toggle("close")
 
-      })
+      });
+    function redirecionarC(){
+      window.location.href="../pages/curriculo.html"
+    };
+    function redirecionarA(){
+      window.location.href="../pages/aboutme.html"
+    }
+    function redirecionarP(){
+      window.location.href="../pages/projetos.html"
+    }
+    function redirecionarCA(){
+      window.location.href="../pages/contato.html"
+    };
+
+  
+
+  
+     
+
+
